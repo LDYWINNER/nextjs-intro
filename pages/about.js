614511302d1cd86248e62d41ba2dc70c16bbb23d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-09-29 16:41:23 -0400

    1.3 Routing

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,10 @@
-export default function About() {
-  return "about us";
+import NavBar from "../components/NavBar";
+
+export default function Potato() {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
